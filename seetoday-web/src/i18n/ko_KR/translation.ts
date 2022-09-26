e89e9f354cf78b1d868094be2ad9ee835752ab28
sshrik <chj878194@naver.com>
2022-09-26 22:30:03 +0900

    fix: remove useless text translation

--- seetoday-web/src/i18n/ko_KR/translation.ts
+++ seetoday-web/src/i18n/ko_KR/translation.ts
@@ -1,12 +1,3 @@
-const text = {
-  main_page_title: '메인 페이지',
-  sub_1_page: '서브 페이지 1',
-  sub_2_page: '서브 페이지 2',
-  sub_3_page: '서브 페이지 3',
-  to_main: '메인 페이지로 이동',
-  to_sub1: '서브 페이지 1로 이동',
-  to_sub2: '서브 페이지 2로 이동',
-  to_sub3: '서브 페이지 3로 이동',
-};
+const text = {};
 
 export default text;
