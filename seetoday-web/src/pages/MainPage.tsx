36bf4ef30e67b265a9da255733ecf05062ca366a
sshrik <chj878194@naver.com>
2022-10-10 21:54:23 +0900

    feat: add jellyfish background components

--- seetoday-web/src/pages/MainPage.tsx
+++ seetoday-web/src/pages/MainPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import background from 'assets/background.png';
+import JellyFish from 'components/background/JellyFish';
 import WaterDrop from 'components/background/WaterDrop';
 import Fish from 'components/Fish';
 import Hook from 'components/Hook';
@@ -23,8 +24,11 @@ const MainPage: React.FC = () => {
       {[...new Array(10)].map((_, index) => (
         <WaterDrop key={`water-${index}`} />
       ))}
+      {[...new Array(10)].map((_, index) => (
+        <JellyFish key={`jelly-${index}`} />
+      ))}
       {[...new Array(5)].map((_, index) => (
-        <Fish key={`water-${index}`} />
+        <Fish key={`fish-${index}`} />
       ))}
       <Hook />
       <HookController />
--- seetoday-web/src/utils/generateRandomNumber.ts
+++ seetoday-web/src/utils/generateRandomNumber.ts
@@ -1,6 +1,6 @@
 const generateRandomNumber = (max: number, min = 0, isCeil = true) => {
-  if (isCeil) return Math.ceil(Math.random() * max) + min;
-  return Math.random() * max + min;
+  if (isCeil) return Math.ceil(Math.random() * (max - min)) + min;
+  return Math.random() * (max - min) + min;
 };
 
 export default generateRandomNumber;
